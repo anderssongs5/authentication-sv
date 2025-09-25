24e2d7f34d7b75d24fbcde32da1df093be53c8b1
Andersson García Sotelo <anderssongarciasotelo@gmail.com>
2025-09-25 10:29:33 -0500

    Changed docker command, added platform to build image, added new endpoint to be excluded from security, added envars in application.yaml

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/powerup/ags/authentication/api/constants/SecurityConstants.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/powerup/ags/authentication/api/constants/SecurityConstants.java
@@ -1,20 +1,26 @@
 package co.com.powerup.ags.authentication.api.constants;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class SecurityConstants {
     
     private SecurityConstants() {
         super();
     }
-    
-    public static final Set<String> EXCLUDED_PATTERNS = Set.of(
+
+    private static final Set<String> EXCLUDED_URLS = Set.of(
             "/v3/api-docs.yaml",
             "/v3/api-docs/**",
             "/webjars/swagger-ui/**",
             "/api/v1/login",
             "/api/v1/introspect",
-            "/api/v1/users/search");
+            "/api/v1/users/search",
+            "/actuator/**",
+            "/prometheus/**");
+
+    public static final Set<String> EXCLUDED_PATTERNS = EXCLUDED_URLS
+            .stream().map(url -> "/auth" + url).collect(Collectors.toSet());
     
     public static final String TOKEN = "token";
     
